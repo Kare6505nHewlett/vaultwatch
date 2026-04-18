4b1f7aa82363e7c7cf0d1f52f1f3b12dc351ea9d
Karen Hewlett <Kare6505nHewlett@users.noreply.github.com>
2026-04-18 17:13:32 +0800

    test(alert): add TestMultiNotifier_AllFail to cover multiple error aggregation

--- internal/alert/multi_test.go
+++ internal/alert/multi_test.go
@@ -57,6 +57,22 @@ func TestMultiNotifier_OneFailsContinues(t *testing.T) {
 	assert.Equal(t, 1, b.called)
 }
 
+func TestMultiNotifier_AllFail(t *testing.T) {
+	errA := errors.New("notifier A failed")
+	errB := errors.New("notifier B failed")
+	a := &fakeNotifier{err: errA}
+	b := &fakeNotifier{err: errB}
+	mn := alert.NewMultiNotifier(zap.NewNop(), a, b)
+
+	err := mn.Send(context.Background(), baseResult())
+
+	// both errors should be surfaced
+	assert.ErrorIs(t, err, errA)
+	assert.ErrorIs(t, err, errB)
+	assert.Equal(t, 1, a.called)
+	assert.Equal(t, 1, b.called)
+}
+
 func TestMultiNotifier_NilLogger(t *testing.T) {
 	n := &fakeNotifier{}
 	mn := alert.NewMultiNotifier(nil, n)
